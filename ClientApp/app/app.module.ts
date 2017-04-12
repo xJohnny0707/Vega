c1f59dc11bad276578db9c5b0df2e1a09ff8428b
Munoz <JOMunoz@livingstonintl.com>
2017-04-12 08:34:05 -0600

    Added Features API and Vehicle-Form Component

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
+
+import { VehicleService } from './services/vehicle.service';
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -14,17 +19,23 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
+        FormsModule,
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path:"vehicles/new", component: VehicleFormComponent},
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ], 
+    providers:[
+        VehicleService
     ]
 })
 export class AppModule {
--- Migrations/20170411222740_IncludeFeatures.Designer.cs
+++ Migrations/20170411222740_IncludeFeatures.Designer.cs
@@ -8,9 +8,10 @@ using vega.Persistence;
 namespace Vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170411222740_IncludeFeatures")]
+    partial class IncludeFeatures
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
